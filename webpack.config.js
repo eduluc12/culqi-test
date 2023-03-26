176f25e0e5d5c0d249aeff4f6000e2b2bb8ad0e8
Eduardo <barron.luccioni@gmail.com>
2023-03-26 14:23:06 -0500

    refactor: allowlist

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
@@ -8,8 +9,11 @@ module.exports = {
         save: './src/lcr/process/save/index.ts',
         transform: './src/lcr/process/transform/index.ts'
     },
+    mode: 'development',
     target: 'node',
-    externals: [nodeExternals()],
+    externals: [nodeExternals({
+        allowlist: ['@nestjs/core', '@redis/client', '@vendia/serverless-express', 'nanoid']
+    })],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
@@ -23,7 +27,7 @@ module.exports = {
     plugins: [
         new webpack.IgnorePlugin({
             checkResource(resource) {
-                const lazyImports = ['@nestjs/microservices', '@nestjs/platform-express', '@nestjs/grahpql', 'cache-manager', 'class-validator', 'class-transformer', 'graphql'];
+                const lazyImports = ['@nestjs/microservices', '@nestjs/platform-express', '@nestjs/websockets/socket-module', '@nestjs/microservices/microservices-module', '@nestjs/grahpql', 'cache-manager', 'class-validator', 'class-transformer', 'graphql'];
                 if (!lazyImports.includes(resource)) {
                     return false;
                 }
